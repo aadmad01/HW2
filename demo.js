8d4cba91fa7c85c870a4478bfcc5ef92e835ff90
aadmad01 <98374062+aadmad01@users.noreply.github.com>
2022-04-19 11:22:09 -0400

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var url = require("url");
+var fs = require("fs");
+
+http.createServer(function (req, res) {
+  // console.log("hi");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("Hello World <br/>");
+  res.write("this is another line <br/>");
+  res.write("The url is " + req.url + "<br/>");
+  res.write("Success");
+  res.write("<hr>");
+  res.end();
+}).listen(8080);
